75cc7a7c22d6afc8d319fa976c13c72bb1873565
asodariyadhrumil <asodariyadhrumil830@gmail.com>
2023-09-11 08:43:39 +0530

    yes

--- h14-appliocation/src/validations/index.js
+++
@@ -1,7 +0,0 @@
-module.exports.categoryValidation =require("./category.validation");
-module.exports.subCategoryValidation =require("./subCategory.validation");
-module.exports.subChildCategoryValidation =require("./subChildCategory.validation");
-module.exports.productValidation =require("./product.validation");
-module.exports.cartValidation =require("./cart.validation");
-module.exports.orderValidation =require("./order.validation");
-module.exports.usertValidation =require("./user.validation");
